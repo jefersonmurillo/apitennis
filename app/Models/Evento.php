50ecf8644ed54cb216a9b002d813e8cd46442837
jmurilloariza <jefersonmurillo@wposs.com>
2019-10-10 12:07:31 -0500

    Correción de softdelete

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
@@ -19,6 +20,9 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Evento extends Model
 {
+
+    use SoftDeletes;
+
     /**
      * The table associated with the model.
      * 
